4517645ddae4808197e6c88b769e549888da11a7
funnydog <funnydog@users.noreply.github.com>
2019-12-12 15:22:33 +0100

    Clean the code at the cost of a little slowdown

--- day12/day12.py
+++ day12/day12.py
@@ -3,60 +3,65 @@
 import re
 import math
 
+class Moon(object):
+    def __init__(self, pos):
+        self.pos = pos
+        self.vel = [0, 0, 0]
+
 pattern = re.compile(r"<x=(-?[0-9]+), y=(-?[0-9]+), z=(-?[0-9]+)>")
 def load_moons(text):
     moons = []
     for line in text.split("\n"):
         m = pattern.search(line)
         if m:
-            moons.append([int(x) for x in m.groups()])
+            moons.append(Moon([int(x) for x in m.groups()]))
     return moons
 
-def update_axis(pos, vel):
+def update_step(moons, axis):
     # update the velocity
-    for i in range(len(pos)):
-        for j in range(i+1, len(pos)):
-            if pos[i] < pos[j]:
-                vel[i] += 1
-                vel[j] -= 1
-            elif pos[i] > pos[j]:
-                vel[i] -= 1
-                vel[j] += 1
+    for i in range(len(moons)):
+        for j in range(i+1, len(moons)):
+            if moons[i].pos[axis] < moons[j].pos[axis]:
+                moons[i].vel[axis] += 1
+                moons[j].vel[axis] -= 1
+            elif moons[i].pos[axis] > moons[j].pos[axis]:
+                moons[i].vel[axis] -= 1
+                moons[j].vel[axis] += 1
             else:
                 pass
 
-    # update the gravity
-    for i in range(len(pos)):
-        pos[i] += vel[i]
+    # update the position
+    for m in moons:
+        m.pos[axis] += m.vel[axis]
 
-def energy_after_steps(moons, steps):
-    pos = []
-    vel = []
-    for i in range(3):
-        p = [m[i] for m in moons]
-        v = [0 for m in moons]
+def energy(moons, steps):
+    for axis in range(3):
         for i in range(steps):
-            update_axis(p, v)
-
-        pos.append(p)
-        vel.append(v)
+            update_step(moons, axis)
 
     energy = 0
-    for j in range(len(moons)):
-        k = sum(abs(vel[i][j]) for i in range(3))
-        p = sum(abs(pos[i][j]) for i in range(3))
+    for m in moons:
+        p = sum(abs(i) for i in m.pos)
+        k = sum(abs(i) for i in m.vel)
         energy += k*p
 
     return energy
 
-def simulate_axis(pos, vel):
-    oldpos = tuple(pos)
-    oldvel = tuple(vel)
+def repeat_axis(moons, axis):
+    pos = [m.pos[axis] for m in moons]
+    vel = [m.vel[axis] for m in moons]
+
     count = 0
     while True:
-        update_axis(pos, vel)
+        update_step(moons, axis)
         count += 1
-        if tuple(pos) == oldpos and tuple(vel) == oldvel:
+
+        exit = True
+        for i, m in enumerate(moons):
+            if m.pos[axis] != pos[i] or m.vel[axis] != vel[i]:
+                exit = False
+                break
+        if exit:
             break
 
     return count
@@ -64,43 +69,32 @@ def simulate_axis(pos, vel):
 def lcm(a, b):
     return a * b // math.gcd(a, b)
 
-def steps_to_state(moons):
-    xcount = simulate_axis(
-        [p[0] for p in moons],
-        [0 for p in moons]
-    )
+def repeat(moons):
+    x = repeat_axis(moons, 0)
+    y = repeat_axis(moons, 1)
+    z = repeat_axis(moons, 2)
+    return lcm(lcm(x, y), z)
 
-    ycount = simulate_axis(
-        [p[1] for p in moons],
-        [0 for p in moons]
-    )
-
-    zcount = simulate_axis(
-        [p[2] for p in moons],
-        [0 for p in moons]
-    )
-    return lcm(lcm(xcount, ycount), zcount)
-
-txt = """
+moons = load_moons("""
 <x=-1, y=0, z=2>
 <x=2, y=-10, z=-7>
 <x=4, y=-8, z=8>
 <x=3, y=5, z=-1>
-"""
-assert energy_after_steps(load_moons(txt), 10) == 179
-assert steps_to_state(load_moons(txt)) == 2772
+""")
+assert energy(moons, 10) == 179
+assert repeat(moons) == 2772
 
-txt = """
+moons = load_moons("""
 <x=-8, y=-10, z=0>
 <x=5, y=5, z=10>
 <x=2, y=-7, z=3>
 <x=9, y=-8, z=-3>
-"""
-assert energy_after_steps(load_moons(txt), 100) == 1940
-assert steps_to_state(load_moons(txt)) == 4686774924
+""")
+assert energy(moons, 100) == 1940
+assert repeat(moons) == 4686774924
 
 with open("input", "rt") as f:
-    txt = f.read()
+    moons = load_moons(f.read())
 
-print("part1:", energy_after_steps(load_moons(txt), 1000))
-print("part2:", steps_to_state(load_moons(txt)))
+print("part1:", energy(moons, 1000))
+print("part2:", repeat(moons))
